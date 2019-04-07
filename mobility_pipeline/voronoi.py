ab029b819954532a521121a8b037496cb9bc2403
cs <cs.temporary@icloud.com>
2019-04-07 17:28:28 +0000

    Remove Outer List from Points to load_voronoi
    The `load_voronoi` function used to accept points with an unnecessary
    outer list like this `[[[lat, long], [lat, long]]]` because that is the
    format of the value of `coordinates` in the voronoi JSON. However, the
    stripping of the unnecessary outer list is better placed in the client
    of `load_voronoi`, leaving `load_voronoi` more versatile and with more
    intuitive arguments.

--- mobility_pipeline/voronoi.py
+++ mobility_pipeline/voronoi.py
@@ -14,7 +14,7 @@ import numpy as np  # type: ignore
 from shapely.geometry import Polygon  # type: ignore
 
 
-def load_voronoi(voronoi_json: List[List[List[float]]]) -> Polygon:
+def load_voronoi(voronoi_json: List[List[float]]) -> Polygon:
     """
 
     Loads Voronoi from JSON of the format:
@@ -22,12 +22,10 @@ def load_voronoi(voronoi_json: List[List[List[float]]]) -> Polygon:
     .. code-block::json
 
         [
-            [
-                [latitude, longitude],
-                [latitude, longitude],
-                ...
-                [latitude, longitude]
-            ]
+            [latitude, longitude],
+            [latitude, longitude],
+            ...
+            [latitude, longitude]
         ]
 
     where each latitude-longitude pair describes a point of the Voronoi
@@ -41,5 +39,5 @@ def load_voronoi(voronoi_json: List[List[List[float]]]) -> Polygon:
         A polygon of the Voronoi partition
 
     """
-    voronoi_points = np.array(voronoi_json[0])
+    voronoi_points = np.array(voronoi_json)
     return Polygon(voronoi_points)
--- tests/src/test_voronoi.py
+++ tests/src/test_voronoi.py
@@ -5,7 +5,7 @@ from mobility_pipeline.voronoi import load_voronoi
 
 
 def test_load_voronoi():
-    voronoi_json = "[[[-5, -5], [-5, 0], [5, 0], [5, -5]]]"
+    voronoi_json = "[[-5, -5], [-5, 0], [5, 0], [5, -5]]"
     polygon = load_voronoi(json.loads(voronoi_json))
     assert polygon.bounds == (-5.0, -5.0, 5.0, 0.0)  # (minx, miny, maxx, maxy)
     assert polygon.area == 50
