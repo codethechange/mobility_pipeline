67824a39739c5f6b6eb8ce7e10e01603e19248dc
cs <cs.temporary@icloud.com>
2019-04-08 05:20:30 +0000

    Handle MultiPolygon Voronoi Cells in load_cell
    A Voronoi cell may be discontiguous, for example if a tower also serves
    an island off the mainland. These are stored in the JSON as
    `MultiPolygon`s. To handle this, `load_cell` now accepts more of the
    JSON so it can access the `type` key to see if the JSON is for a
    `MultiPolygon` or a `Polygon`. Processing differs by type, but in any
    event a `MultiPolygon` is returned. It will have only one member if the
    JSON was for a `Polygon`.

--- mobility_pipeline/voronoi.py
+++ mobility_pipeline/voronoi.py
@@ -6,38 +6,124 @@ is called a cell,
 the cell contains exactly one seed, and every point in the cell is
 closer to the cell's seed than it is to any other seed. For more
 information, see https://en.wikipedia.org/wiki/Voronoi_diagram.
-
 """
 
-from typing import List
+
+from typing import List, Union, cast
+from mypy_extensions import TypedDict
 import numpy as np  # type: ignore
-from shapely.geometry import Polygon  # type: ignore
+from shapely.geometry import Polygon, MultiPolygon  # type: ignore
 
 
-def load_cell(voronoi_json: List[List[float]]) -> Polygon:
-    """
+def json_to_polygon(points_json: List[List[float]]) -> Polygon:
+    """Loads a Polygon from a JSON of points
 
-    Loads Voronoi from JSON of the format:
+    Loads Polygon from a JSON of the format:
 
     .. code-block::json
 
-        [
-            [latitude, longitude],
-            [latitude, longitude],
-            ...
-            [latitude, longitude]
-        ]
+            [
+                [latitude, longitude],
+                [latitude, longitude],
+                ...
+                [latitude, longitude]
+            ]
 
-    where each latitude-longitude pair describes a point of the Voronoi
-    tessellation.
+    where each latitude-longitude pair describes a point defining the boundary
+    of the polygon.
 
     Args:
-        voronoi_json: The points that define the boundary of the Voronoi
-            partition
+        points_json: The points that define the boundary of the polygon
 
     Returns:
-        A polygon of the Voronoi partition
-
+        A polygon
     """
-    voronoi_points = np.array(voronoi_json)
-    return Polygon(voronoi_points)
+    pts = np.array(points_json)
+    return Polygon(pts)
+
+
+class VoronoiCell(TypedDict):
+    """This class describes is for type hinting Voronoi cell JSONs
+    """
+    type: str
+    # Helpful explanation of type Union: https://stackoverflow.com/a/51304069
+    coordinates: Union[List[List[List[float]]], List[List[List[List[float]]]]]
+
+
+def load_cell(cell_json: VoronoiCell) -> MultiPolygon:
+    """Loads a Voronoi cell from JSON in ``Polygon`` or ``MultiPolygon`` format
+
+    Loads Voronoi cell from a JSON of the ``Polygon`` format:
+
+    .. code-block::json
+
+        {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [latitude, longitude],
+                    [latitude, longitude],
+                    ...
+                    [latitude, longitude]
+                ]
+            ]
+        }
+
+    or of the ``MultiPolygon`` format:
+
+    .. code-block::json
+
+        {
+            "type": "MultiPolygon",
+            "coordinates": [
+                [
+                    [
+                        [latitude, longitude],
+                        [latitude, longitude],
+                        ...
+                        [latitude, longitude]
+                    ]
+                ],
+                [
+                    [
+                        [latitude, longitude],
+                        [latitude, longitude],
+                        ...
+                        [latitude, longitude]
+                    ]
+                ],
+                ...
+                [
+                    [
+                        [latitude, longitude],
+                        [latitude, longitude],
+                        ...
+                        [latitude, longitude]
+                    ]
+                ]
+            ]
+        }
+
+    where each latitude-longitude pair describes a point of the Voronoi
+    tessellation. If the JSON is in the ``Polygon`` format, a
+    ``shapely.geometry.MultiPolygon`` object will be returned where the
+    ``MultiPolygon`` has one member, the described polygon.
+
+    The value of the ``type`` key is used to distinguish ``Polygon`` and
+    ``MultiPolygon`` formats.
+
+    Args:
+        cell_json: The points that define the boundary of the Voronoi
+            cell
+
+    Returns:
+        A polygon of the Voronoi cell
+    """
+    # We typecast because the value of `type` tells us the type of `coordinates`
+    if cell_json['type'] == 'Polygon':
+        coors = cast(List[List[List[float]]], cell_json['coordinates'])
+        pol = json_to_polygon(coors[0])
+        return MultiPolygon([pol])
+    pols = [json_to_polygon(pol_json[0]) for pol_json in
+            cast(List[List[List[List[float]]]], cell_json['coordinates'])]
+    return MultiPolygon(pols)
--- tests/src/test_voronoi.py
+++ tests/src/test_voronoi.py
@@ -1,11 +1,59 @@
 # pragma pylint: disable=missing-docstring
 
 import json
+from shapely.geometry import Point
 from mobility_pipeline.voronoi import load_cell
 
 
-def test_load_cell():
-    voronoi_json = "[[-5, -5], [-5, 0], [5, 0], [5, -5]]"
+def test_load_cell_polygon():
+    voronoi_json = """
+    {
+        "type": "Polygon",
+        "coordinates": [
+            [
+                [-5, -5],
+                [-5, 0],
+                [5, 0],
+                [5, -5]
+            ]
+        ]
+    }
+    """
     polygon = load_cell(json.loads(voronoi_json))
     assert polygon.bounds == (-5.0, -5.0, 5.0, 0.0)  # (minx, miny, maxx, maxy)
     assert polygon.area == 50
+
+
+def test_load_cell_multipolygon():
+    voronoi_json = """
+    {
+        "type": "MultiPolygon",
+        "coordinates": [
+            [
+                [
+                    [-5, -5],
+                    [-5, 0],
+                    [5, 0],
+                    [5, -5]
+                ]
+            ],
+            [
+                [
+                    [5, 0],
+                    [10, 0],
+                    [10, 5],
+                    [5, 5]
+                ]
+            ]
+        ]
+    }
+    """
+    polygon = load_cell(json.loads(voronoi_json))
+    print(len(polygon.geoms))
+    assert polygon.bounds == (-5.0, -5.0, 10.0, 5.0)  # (minx, miny, maxx, maxy)
+    assert polygon.area == 50 + 25
+    assert polygon.contains(Point(0, -2))
+    assert polygon.contains(Point(7, 3))
+    assert not polygon.contains(Point(0, 0))  # Boundaries are not contained
+    assert not polygon.contains(Point(5, 0))
+    assert not polygon.contains(Point(0, 5))
