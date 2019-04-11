17da8766b4035aa0cde1078286d931e370ace482
cs <cs.temporary@icloud.com>
2019-04-11 03:15:00 +0000

    Remove debugging print statement from voronoi tests

--- tests/src/test_voronoi.py
+++ tests/src/test_voronoi.py
@@ -49,7 +49,6 @@ def test_load_cell_multipolygon():
     }
     """
     polygon = load_cell(json.loads(voronoi_json))
-    print(len(polygon.geoms))
     assert polygon.bounds == (-5.0, -5.0, 10.0, 5.0)  # (minx, miny, maxx, maxy)
     assert polygon.area == 50 + 25
     assert polygon.contains(Point(0, -2))
