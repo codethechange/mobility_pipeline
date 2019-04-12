5ee6c29186f7ed10d0a4b30c0b1ebb0d90cc5b9e
cs <cs.temporary@icloud.com>
2019-04-12 22:26:05 +0000

    Update data interface for new data files

--- data_interface.py
+++ data_interface.py
@@ -5,22 +5,24 @@ This file is specific to the data files we are using and their format.
 
 import json
 import numpy as np
+from typing import List
+from shapely.geometry import MultiPolygon
 from mobility_pipeline.voronoi import load_cell
 
 DATA_PATH = "data/"
 
-TOWERS_PATH = "%sbrazil-mobility/towers_br.csv" % DATA_PATH
-VORONOI_PATH = "%sbrazil-mobility/voronoi_br.json" % DATA_PATH
+TOWERS_PATH = "%sbrazil-towers-voronoi-mobility/towers_br.csv" % DATA_PATH
+VORONOI_PATH = "%sbrazil-towers-voronoi-mobility/brazil-voronoi.json" % DATA_PATH
 
 
-def load_cells():
+def load_cells() -> List[MultiPolygon]:
     with open(VORONOI_PATH, 'r') as f:
         raw_json = json.loads(f.read())
     cells = [load_cell(feature['geometry']) for feature in raw_json['features']]
     return cells
 
 
-def load_towers():
+def load_towers() -> np.ndarray:
     towers_mat = np.genfromtxt(TOWERS_PATH, delimiter=',')
     towers_mat = towers_mat[1:, 1:]
     return towers_mat
