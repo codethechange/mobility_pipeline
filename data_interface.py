248a53df3f6cc0e46dc46dee859c480e246c6f13
cs <cs.temporary@icloud.com>
2019-04-13 05:33:58 +0000

    Add data interface for loading mobility data

--- data_interface.py
+++ data_interface.py
@@ -4,15 +4,18 @@ This file is specific to the data files we are using and their format.
 """
 
 import json
+import re
+import csv
 import numpy as np
 from typing import List
 from shapely.geometry import MultiPolygon
 from mobility_pipeline.voronoi import load_cell
 
-DATA_PATH = "data/"
+DATA_PATH = "data/brazil-towers-voronoi-mobility/"
 
-TOWERS_PATH = "%sbrazil-towers-voronoi-mobility/towers_br.csv" % DATA_PATH
-VORONOI_PATH = "%sbrazil-towers-voronoi-mobility/brazil-voronoi.json" % DATA_PATH
+TOWERS_PATH = "%stowers_br.csv" % DATA_PATH
+VORONOI_PATH = "%sbrazil-voronoi.json" % DATA_PATH
+MOBILITY_PATH = "%smobility_matrix_20150201.csv" % DATA_PATH
 
 
 def load_cells() -> List[MultiPolygon]:
@@ -26,3 +29,17 @@ def load_towers() -> np.ndarray:
     towers_mat = np.genfromtxt(TOWERS_PATH, delimiter=',')
     towers_mat = towers_mat[1:, 1:]
     return towers_mat
+
+
+def load_mobility() -> np.ndarray:
+    num_re = re.compile('[0-9]+')
+
+    def get_int(x):
+        return int(num_re.search(x).group(0))
+
+    with open(MOBILITY_PATH, newline='') as f:
+        mobility_csv = csv.reader(f)
+        next(mobility_csv)  # Skip header line of CSV
+        mobility_csv = [[get_int(ori), get_int(dst), count]
+                        for date, ori, dst, count in mobility_csv]
+    return np.array(mobility_csv)
