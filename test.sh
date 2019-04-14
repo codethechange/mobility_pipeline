123c3714cbf091871a2fdb83838eed2326b43b96
cs <cs.temporary@icloud.com>
2019-04-14 01:03:32 +0000

    Specify search paths for tests

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+saved=$PYTHONPATH
+export PYTHONPATH="./mobility_pipeline/:$PYTHONPATH"
 python -m pytest
 
 find mobility_pipeline -name '*.py' | xargs pylint
@@ -8,3 +9,4 @@ find tests/src -name '*.py' | xargs pylint
 python -m mypy mobility_pipeline
 
 liccheck -s strategy.ini -r requirements.txt
+export PYTHONPATH="saved"
