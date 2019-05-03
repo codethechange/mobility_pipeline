863eac02bf2e6d41334503ea3094e5d706a8d01d
cs <cs.temporary@icloud.com>
2019-05-03 04:53:53 +0000

    Fix overlap computation by not dividing by enclosing area

--- mobility_pipeline/lib/overlap.py
+++ mobility_pipeline/lib/overlap.py
@@ -1,17 +1,9 @@
 from shapely.geometry import Polygon
 
 def computeOverlap(enclosing, polygons):
-    areaEnclosed = enclosing.area
     overlapPercentages = []
     for idx in range(len(polygons)):
         intersection = polygons[idx].intersection(enclosing)
-        curr_percent = intersection.area / areaEnclosed
+        curr_percent = intersection.area / polygons[idx].area
         overlapPercentages.append(curr_percent)
     return overlapPercentages
-
-polygon1 = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
-polygon2 = Polygon([(0, 0), (0, 3), (3, 3), (3, 0)])
-polygons = [polygon1, polygon2]
-enclosing = Polygon([(0, 0), (0, 10), (10, 10), (10, 0)])
-
-print(computeOverlap(enclosing, polygons))
