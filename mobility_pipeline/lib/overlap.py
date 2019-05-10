7c86e408208fc45325769c7cbc67c191c32ac46b
Andy Jin <andyjin@stanford.edu>
2019-05-09 20:06:22 -0700

    Updated compute_overlap function (5.9)  Please enter the commit message for your changes. Lines starting

--- mobility_pipeline/lib/overlap.py
+++ mobility_pipeline/lib/overlap.py
@@ -1,9 +1,10 @@
-from shapely.geometry import Polygon
+"""Determines overlap between a shape and a list of polygons """
 
-def computeOverlap(enclosing, polygons):
-    overlapPercentages = []
-    for idx in range(len(polygons)):
-        intersection = polygons[idx].intersection(enclosing)
-        curr_percent = intersection.area / polygons[idx].area
-        overlapPercentages.append(curr_percent)
-    return overlapPercentages
+def compute_overlap(enclosing, polygons):
+    """Computes the percentage overlap of enclosing with each polygon in list of polygons"""
+    overlap_percentages = []
+    for _, polygon in enumerate(polygons):
+        intersection = polygon.intersection(enclosing)
+        curr_percent = intersection.area / polygon.area
+        overlap_percentages.append(curr_percent)
+    return overlap_percentages
