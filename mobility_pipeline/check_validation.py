4612a2a2cbee35a48e1f586c2d05e1f92f6f58cb
cs <cs.temporary@icloud.com>
2019-05-17 20:41:40 +0000

    Create script to validate data files
    The following data files are checked:
    * Towers file
    * Voronoi file
    * Mobility file

---
+++ mobility_pipeline/check_validation.py
@@ -0,0 +1,90 @@
+"""Script that checks the validity of data files
+"""
+
+import csv
+from mobility_pipeline.data_interface import TOWERS_PATH, MOBILITY_PATH, \
+    load_cells, load_towers
+from mobility_pipeline.lib.validate import validate_mobility, \
+    validate_voronoi, validate_tower_cells_aligned, \
+    validate_tower_index_name_aligned
+
+
+def validate_data_files() -> bool:
+    """Check the validity of data files
+
+    Data files validated:
+
+    * Towers file at :py:const:`mobility_pipeline.data_interface.TOWERS_PATH`
+    * Voronoi file at :py:const:`mobility_pipeline.data_interface.VORONOI_PATH`
+    * Mobility file at
+      :py:const:`mobility_pipeline.data_interface.MOBILITY_PATH`
+
+    Returns:
+        ``True`` if all files are valid, ``False`` otherwise.
+    """
+    # Load and validate towers
+    f = None
+    try:
+        f = open(TOWERS_PATH, 'r')
+        towers_csv = csv.reader(f)
+    except (FileNotFoundError, IOError) as e:
+        msg = repr(e)
+        print(f'INVALID: error opening towers file at {TOWERS_PATH}: {msg}')
+        return False
+    else:
+        print('SUCCESS: Towers file opened')
+        align_error = validate_tower_index_name_aligned(towers_csv)
+        if align_error:
+            print(f'INVALID tower index alignment: {align_error}')
+            return False
+        else:
+            print('SUCCESS: Tower indices aligned')
+        towers = load_towers()
+    finally:
+        if f:
+            f.close()
+
+    # Load and validate Voronoi
+    voronoi_errs = validate_voronoi()
+    if voronoi_errs:
+        print(f'INVALID Voronoi: {voronoi_errs}')
+        return False
+    else:
+        print('SUCCESS: Voronoi valid')
+    voronoi = load_cells()
+
+    # Check towers and Voronoi aligned
+    tower_voronoi_align_err = validate_tower_cells_aligned(voronoi, towers)
+    if tower_voronoi_align_err:
+        print(f'INVALID tower-Voronoi alignment: {tower_voronoi_align_err}')
+        return False
+    else:
+        print('SUCCESS: Tower and Voronoi indices aligned')
+
+    # Validate mobility data
+    try:
+        f = open(MOBILITY_PATH, 'r')
+        mobility_csv = csv.reader(f)
+    except (FileNotFoundError, IOError) as e:
+        msg = repr(e)
+        print(f'INVALID: error opening mobility file at {MOBILITY_PATH}: {msg}')
+        return False
+    else:
+        print('SUCCESS: Mobility data loaded')
+        mobility_err = validate_mobility(list(mobility_csv)[1:])
+        if mobility_err:
+            print(f'INVALID mobility data: {mobility_err}')
+            return False
+        else:
+            print('SUCCESS: Mobility data valid')
+    finally:
+        if f:
+            f.close()
+    return True
+
+
+if __name__ == '__main__':
+    if validate_data_files():
+        exit(0)
+    else:
+        exit(1)
