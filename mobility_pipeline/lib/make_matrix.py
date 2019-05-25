ba57c8b578b94c50134f6e9e2e223e5834f7ed57
cs <cs.temporary@icloud.com>
2019-05-25 05:02:03 +0000

    Add docstrings and fix style in make_matrix

--- mobility_pipeline/lib/make_matrix.py
+++ mobility_pipeline/lib/make_matrix.py
@@ -33,20 +33,14 @@ This strategy is explained by Mike Fabrikant at UNICEF:
 https://medium.com/@mikefabrikant/cell-towers-chiefdoms-and-anonymized-call-detail-records-a-guide-to-creating-a-mobility-matrix-d2d5c1bafb68
 """
 
+from collections.abc import Sequence
+from typing import Tuple, Dict, List
 import numpy as np  # type: ignore
 import pandas as pd  # type: ignore
-
-from typing import List, Union, cast
-from mypy_extensions import TypedDict
-import numpy as np  # type: ignore
-from shapely.geometry import Polygon, MultiPolygon 
-from shapely.strtree import STRtree
+from shapely.strtree import STRtree  # type: ignore
+from shapely.geometry import MultiPolygon  # type: ignore
 from lib.overlap import compute_overlap
-# from rtree import index
-from collections.abc import Sequence
 
-
-from matplotlib import pyplot as plt # FOR TESTING PURPOSES
 
 def make_tower_tower_matrix(mobility: pd.DataFrame, n_towers: int) \
         -> np.ndarray:
@@ -76,35 +70,107 @@ def make_tower_tower_matrix(mobility: pd.DataFrame, n_towers: int) \
     np_array = mobility['COUNT'].values
     return np.reshape(np_array, (n_towers, n_towers))
 
-#Constructs Rtree from polygons, dict from polygon to index in original sequence
-def generate_rtree(polygons: Sequence):
+
+def generate_rtree(polygons: Sequence) -> Tuple[STRtree,
+                                                Dict[Tuple[tuple, ...], int]]:
+    """Helper function that builds an RTree from MultiPolygons
+
+    The Rtree is built from MultiPolygons using
+    :py:class:`shapely.strtree.STRtree`. Since the RTree returns the overlapping
+    MultiPolygons, we need a way to retrieve the polygon's index. We do this
+    with a dictionary from the exterior coordinates (`Polygon.exterior.coords`)
+    of every Polygon in the MultiPolygon to the MultiPolygon's index in the
+    provided Sequence.
+
+    Specifically, you can generate the key for a given MultiPolygon ``mpoly``
+    like so:
+
+    .. code-block:: python
+
+        key = tuple([tuple(p.exterior.coords) for p in mpoly])
+
+    Args:
+        polygons: A Sequence of MultiPolygons. Must be iterable and able to be
+            passed to the ``STRtree`` constructor. Iteration must be
+            deterministic.
+
+    Returns:
+        A tuple of the RTree and the index mapping dictionary.
+
+    """
     tree = STRtree(polygons)
     index_mapping = {}
     for i, mpoly in enumerate(polygons):
         index_mapping[tuple([tuple(p.exterior.coords) for p in mpoly])] = i
-    return (tree, index_mapping)
-        
+    return tree, index_mapping
 
-#makes a matrix from first input to second input
-def make_a_to_b_matrix(a_cells: Sequence, b_cells: Sequence) -> np.ndarray:
-    #make zeroed matrix
+
+def make_a_to_b_matrix(a_cells: List[MultiPolygon],
+                       b_cells: List[MultiPolygon]) -> np.ndarray:
+    """Create an overlap matrix from sequence A to B
+
+    Computes for every pair of MultiPolygons between A and B, the fraction of
+    the MultiPolygon in B that is covered by the one in A. We use an RTree to
+    reduce the number of overlaps we have to compute by only computing overlaps
+    between MultiPolygons that have overlapping bounding boxes.
+
+    Args:
+        a_cells: Sequence A of MultiPolygons
+        b_cells: Sequence B of MultiPolygons
+
+    Returns:
+        A matrix with row indices that correspond to the indices of B and column
+        indices that correspond to the indices of A. Every element at row
+        ``i`` and column ``j`` in the matrix represents the fraction of the
+        MultiPolygon in B at index ``i`` that overlaps with the MultiPolygon in
+        A at index ``j``.
+    """
     mat = np.zeros((len(a_cells), len(b_cells)))
-    #generate tree and mapping
     a_rtree, tree_index_mapping = generate_rtree(a_cells)
     for i, bcell in enumerate(b_cells):
-        #find potential overlapping polys using opposing rtree
         overlapping_cells = a_rtree.query(bcell)
         for acell in overlapping_cells:
-            #compute true overlap and update corresponding entry in matrix
+            # compute true overlap and update corresponding entry in matrix
             coords = tuple([tuple(pol.exterior.coords) for pol in acell])
             mat[tree_index_mapping[coords]][i] = compute_overlap(bcell, acell)
     return mat
 
-def make_tower_to_admin_matrix(admin_cells: Sequence, tower_cells: Sequence) -> np.ndarray:
+
+def make_tower_to_admin_matrix(admin_cells: List[MultiPolygon],
+                               tower_cells: List[MultiPolygon])\
+        -> np.ndarray:
+    """Compute the tower-to-admin matrix.
+
+    This is a wrapper function for :py:meth:`make_a_to_b_matrix`, with matrices
+    A and B as denoted for each argument.
+
+    Args:
+        admin_cells: Sequence of administrative regions; used as matrix A.
+        tower_cells: Sequence of Voronoi cells; used as matrix B.
+
+    Returns:
+        The tower-admin matrix.
+    """
     return make_a_to_b_matrix(tower_cells, admin_cells)
 
-def make_admin_to_tower_matrix(admin_cells: Sequence, tower_cells: Sequence) -> np.ndarray:
+
+def make_admin_to_tower_matrix(tower_cells: List[MultiPolygon],
+                               admin_cells: List[MultiPolygon]) \
+        -> np.ndarray:
+    """Compute the admin-to-tower matrix.
+
+    This is a wrapper function for :py:meth:`make_a_to_b_matrix`, with matrices
+    A and B as denoted for each argument.
+
+    Args:
+        tower_cells: Sequence of Voronoi cells; used as matrix A.
+        admin_cells: Sequence of administrative regions; used as matrix B.
+
+    Returns:
+        The admin-tower matrix.
+    """
     return make_a_to_b_matrix(admin_cells, tower_cells)
+
 
 def make_admin_admin_matrix(tower_tower: np.ndarray, tower_admin: np.ndarray,
                             admin_tower: np.ndarray) -> np.ndarray:
