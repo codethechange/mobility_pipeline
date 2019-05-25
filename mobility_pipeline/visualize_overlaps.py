f8b09a97ab0a37230c61f166e527099914e7d547
cs <cs.temporary@icloud.com>
2019-05-25 19:12:32 +0000

    Style and docstring fixes for visualize_overlaps

--- mobility_pipeline/visualize_overlaps.py
+++ mobility_pipeline/visualize_overlaps.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""Plots one Voronoi cell on top of all the administrative regions. The admins
+that intersect the Voronoi cell are colored by index and have the associated
+values from the tower-to-admin matrix printed. This lets you check that the
+matrix values seem reasonable.
+"""
+
 from matplotlib import pyplot as plt  # type: ignore
 from lib.make_matrix import make_a_to_b_matrix
 from data_interface import load_admin_cells, load_cells
@@ -8,21 +14,35 @@ from descartes import PolygonPatch  # type: ignore
 from random import uniform as rand
 
 
-def plot_polygon(axes: plt.axes, polygon: MultiPolygon, color, _label="") -> None:
-    patch = PolygonPatch(polygon, facecolor=color, edgecolor=[0, 0, 0], alpha=0.3, label=_label)
+I_TOWER_TO_COLOR = 1
+"""Index of Voronoi cell to show."""
+
+
+def plot_polygon(axes: plt.axes, polygon: MultiPolygon, color, _label="") \
+        -> None:
+    """Plot a polygon (or multipolygon) with matplotlib
+
+    Args:
+        axes: The matplotlib axes to plot on
+        polygon: The polygon to plot
+        color: Color to use for shading the polygon
+        _label: Label for the polygon that will be displayed in the legend
+
+    Returns:
+        None
+    """
+    patch = PolygonPatch(polygon, facecolor=color, edgecolor=[0, 0, 0],
+                         alpha=0.3, label=_label)
     axes.add_patch(patch)
 
 
-def test_make_matrix():
+if __name__ == '__main__':
     admin_cells = load_admin_cells()
     tower_cells = load_cells()
-    # mat1 = make_tower_admin_matrix(admin_cells, tower_cells)
-    print("before")
     mat = make_a_to_b_matrix(tower_cells, admin_cells)
-    index = 1
-    for i in range(len(mat[:, index])):
-        if mat[i, index] != 0:
-            print(str(i), mat[i, index])
+    for i in range(len(mat[:, I_TOWER_TO_COLOR])):
+        if mat[i, I_TOWER_TO_COLOR] != 0:
+            print(str(i), mat[i, I_TOWER_TO_COLOR])
     plt.ioff()
     fig = plt.figure()
     # (left, bottom, width, height) in units of fractions of figure dimensions
@@ -30,17 +50,14 @@ def test_make_matrix():
     ax.set_aspect(1)
 
     for i, admin in enumerate(admin_cells):
-        if mat[i, index] != 0:
-            plot_polygon(ax, admin, [rand(0,1), rand(0,1), rand(0,1)], str(i))
+        if mat[i, I_TOWER_TO_COLOR] != 0:
+            plot_polygon(ax, admin, [rand(0, 1), rand(0, 1), rand(0, 1)],
+                         str(i))
         else:
             plot_polygon(ax, admin, [0.1, 0, 0])
-    plot_polygon(ax, tower_cells[index], [0, 0, 0])
+    plot_polygon(ax, tower_cells[I_TOWER_TO_COLOR], [0, 0, 0])
 
     ax.relim()
     ax.autoscale_view()
     plt.legend()
     plt.show()
-
-
-if __name__ == '__main__':
-    test_make_matrix()
