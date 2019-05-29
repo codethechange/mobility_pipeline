6065eb94bd2b359a77ebdb8afc44706e4959932d
Laikh Tewari <laikhtewari1@gmail.com>
2019-05-28 22:02:52 -0700

    again

--- mobility_pipeline/data_interface.py
+++ mobility_pipeline/data_interface.py
@@ -52,9 +52,9 @@ def convert_shape_to_json() -> None:
     fields = reader.fields[1:]
     field_names = [field[0] for field in fields]
     buffer = []
-    for sr in reader.shapeRecords():
-        atr = dict(zip(field_names, sr.record))
-        geom = sr.shape.__geo_interface__
+    for shape_record in reader.shapeRecords():
+        atr = dict(zip(field_names, shape_record.record))
+        geom = shape_record.shape.__geo_interface__
         buffer.append(dict(type="Feature", geometry=geom, properties=atr))
     # write the GeoJSON file
     geojson = open(ADMIN_PATH, "w")
